000297d9511cf1aa2cf0bff705046be81595aab4
nimo731 <patience.karanja@student.moringaschool.com>
2025-03-11 12:24:52 +0300

    filtering

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+// findMatching
+function findMatching(drivers,searchName){
+    return drivers.filter(driver => driver.toLowerCase()===searchName.toLowerCase());
+}
+
+let drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+//fuzzyMatch
+function fuzzyMatch (drivers,letters){
+return drivers.filter(driver =>driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+//matchName
+function matchName (drivers,searchName){
+    return drivers.filter(driver => driver.name === searchName);
+}
